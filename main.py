e0cee8d4defb102aa871356f6e91f84cf0c74e1b
denisahmetov <denis.akhmetov.dev@gmail.com>
2023-12-10 15:56:03 +0600

    add callback get_users

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from aiogram.types import Message
 from src.config.default import bot_token
 from src.database.db_queries import create_user, get_all_users
 from src.database.db_tables import db_tables
+from src.quastionnaire.admin_router import admin_router
 
 bot = Bot(token=bot_token)
 dp = Dispatcher()
@@ -15,6 +16,8 @@ dp = Dispatcher()
 async def bot_start():
 
     await db_tables()
+
+    dp.include_router(admin_router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     try:
@@ -33,13 +36,6 @@ async def start_handler(message: Message):
     else:
         name = first_name
     await create_user(user_id=user_id, user_name=name)
-
-    all_users = await get_all_users()
-    if all_users:
-        users_info = "\n".join([f"ID: {user[0]}, tg_id: {user[1]}, Name: {user[2]}" for user in all_users])
-        await message.answer(f"Все пользователи:\n{users_info}")
-    else:
-        await message.answer("No users found.")
 
     await message.answer(f"Привет, {message.from_user.first_name}")
 
--- quastionnaire/admin_router.py
+++ quastionnaire/admin_router.py
@@ -1,8 +1,10 @@
-from aiogram import Router
+from aiogram import Router, F, Bot
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from src.config.default import admin1
+from src.database.db_queries import get_all_users
+from src.quastionnaire.keyboards.admin_kbs import admin_main_ikb
 
 admin_router = Router()
 
@@ -10,6 +12,19 @@ admin_router = Router()
 @admin_router.message(Command("admin"))
 async def get_admin_permissions(message: Message):
     if int(admin1) == message.from_user.id:
-        await message.answer("Доступ администратора получен", reply_markup=...)
+        await message.answer("Доступ администратора получен", reply_markup=admin_main_ikb)
     else:
         await message.answer("Я тебя не понимаю. Для того, чтобы узнать что я умею введи /help")
+
+
+@admin_router.callback_query(F.data == "get_users")
+async def get_users_callback(callback: CallbackQuery, bot: Bot):
+    all_users = await get_all_users()
+    if all_users:
+        users_info = "\n".join([f"ID: {user[0]}, tg_id: {user[1]}, Name: {user[2]}" for user in all_users])
+        await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
+        await callback.message.answer(f"Все пользователи:\n{users_info}", reply_markup=admin_main_ikb)
+
+    else:
+        await callback.answer("No users found.")
+    await callback.answer("")
